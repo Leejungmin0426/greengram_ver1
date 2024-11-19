e1a6e20e7c30001aa6be2aba2f0e2f07c155a996
Leejungmin <skdmldlgpdus0426@gmail.com>
2024-11-19 17:31:52 +0900

    greengram_ver1_1

---
+++ src/main/java/com/green/geengramver1/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.green.geengramver1.user;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class UserMapper {
+}
---
+++ src/test/java/com/green/geengramver1/GeenGramVer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.geengramver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeenGramVer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
