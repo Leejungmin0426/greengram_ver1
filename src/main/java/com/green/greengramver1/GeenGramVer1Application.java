1f89b0d8d76a352d0197b4859f3e095175e9839d
Leejungmin <skdmldlgpdus0426@gmail.com>
2024-11-20 17:59:15 +0900

    greengram_ver1_2

---
+++ src/main/java/com/green/greengramver1/GeenGramVer1Application.java
@@ -0,0 +1,13 @@
+package com.green.greengramver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeenGramVer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeenGramVer1Application.class, args);
+    }
+
+}
